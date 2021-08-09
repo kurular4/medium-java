ff70f4ad52a2b453c1bbcfaa83dd8fff2fb2f2ed
kurular4 <kurular4@gmail.com>
2021-08-09 21:02:25 +0300

    Delay queue codes added

---
+++ delay-queue/src/main/java/com/omer/medium/DelayedItem.java
@@ -0,0 +1,29 @@
+package com.omer.medium;
+
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+public class DelayedItem implements Delayed {
+    private String name;
+    private long startingTime;
+
+    public DelayedItem(String name, long delay) {
+        this.name = name;
+        this.startingTime = System.currentTimeMillis() + delay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {
+        long delay = startingTime - System.currentTimeMillis();
+        return unit.toMillis(delay);
+    }
+
+    @Override
+    public int compareTo(Delayed o) {
+        return Long.compare(startingTime, ((DelayedItem) o).startingTime);
+    }
+}
---
+++ delay-queue/src/main/java/com/omer/medium/Demo.java
@@ -0,0 +1,27 @@
+package com.omer.medium;
+
+import java.util.concurrent.DelayQueue;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        DelayQueue<DelayedItem> delayedItems = new DelayQueue<>();
+        DelayedItem delayedItem1 = new DelayedItem("item1", 1000);
+        DelayedItem delayedItem2 = new DelayedItem("item2", 500);
+        DelayedItem delayedItem3 = new DelayedItem("item3", 750);
+        DelayedItem delayedItem4 = new DelayedItem("item3", 1250);
+
+        delayedItems.add(delayedItem1);
+        delayedItems.add(delayedItem2);
+        delayedItems.add(delayedItem3);
+        delayedItems.add(delayedItem4);
+
+        while (!delayedItems.isEmpty()) {
+            try {
+                System.out.println(delayedItems.take().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
