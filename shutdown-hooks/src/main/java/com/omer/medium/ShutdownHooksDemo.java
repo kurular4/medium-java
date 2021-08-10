7a1374cbd1a893a2cba2048154c3b868337bdb99
Ömer Faruk Kurular <kurular4@gmail.com>
2021-08-10 20:36:05 +0300

    Update ShutdownHooksDemo.java

--- shutdown-hooks/src/main/java/com/omer/medium/ShutdownHooksDemo.java
+++ shutdown-hooks/src/main/java/com/omer/medium/ShutdownHooksDemo.java
@@ -3,6 +3,5 @@ package com.omer.medium;
 public class ShutdownHooksDemo {
     public static void main(String[] args) {
         Runtime.getRuntime().addShutdownHook(new CustomShutdownHook(new NetworkService()));
-
     }
 }
